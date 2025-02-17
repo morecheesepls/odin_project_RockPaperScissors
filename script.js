e17a11f0f321ca3c37c7229bae875d8f7fc40b69
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-17 10:59:21 -0800

    Improvements
    - Added comments in code.
    - Added variables for score and selections.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
+function getComputerChoice() { // Logic for the computer's selection.
+    let computerChoice = Math.floor(Math.random() * 3) + 1; // A random number is chosen between 1 and 3.
     console.log(`A random number between 1 and 3 is selected: ${computerChoice}`);
-    if (computerChoice === 1) {
+    if (computerChoice === 1) { // Converts the numerical selection to a string.
         computerChoice = "Rock";
     } else if (computerChoice === 2) {
         computerChoice = "Paper";
@@ -14,8 +14,8 @@ function getComputerChoice() {
 
 console.log(`Computer selection stored: ${getComputerChoice()}`);
 
-// function getHumanChoice() {
-//     let humanChoice = prompt("Please make your selection: ", " ");
+// function getHumanChoice() { // Logic for the user's selection.
+//     let humanChoice = prompt("Please choose "Rock", "Paper", or "Scissors": ", " "); // Prompts the user to make a selection.
 //     console.log(`The user has selected ${humanChoice}`);
     
 //     humanChoice = humanChoice.toLowerCase();
@@ -28,4 +28,6 @@ console.log(`Computer selection stored: ${getComputerChoice()}`);
 
 let humanScore = 0;
 let computerScore = 0;
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
 
