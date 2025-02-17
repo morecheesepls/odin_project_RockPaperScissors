1693fb3998b2b6c9edb5bd15a47f22d31a12e26d
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-17 12:48:31 -0800

    Round Logic Function
    - Added function with logic for 1 round of play.
    - Logic is incomplete.
    - I am adding a commit here as I think I can condense the function into less code.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerSelection() { // Logic for the computer's selection.
     let computerSelection = Math.floor(Math.random() * 3) + 1; // A random number is chosen between 1 and 3.
     
@@ -14,14 +17,27 @@ function getComputerSelection() { // Logic for the computer's selection.
 
 function getHumanChoice() { // Logic for the user's selection.
     let humanChoice = prompt("Please choose 'Rock', 'Paper', or 'Scissors': ", " ").toLowerCase(); // Prompts the user to make a selection and converts the response to lowercase.
-    
+
     return humanChoice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
 const computerChoice = getComputerSelection();
 const humanChoice = getHumanChoice();
+
+function playRound(humanChoice, computerChoice) { // Logic for the response comparisons.
+    IF (humanChoice === computerChoice) {
+        console.log(`You selected ${humanChoice} and the computer selected ${computerChoice}. This round is a tie!`);
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log(`${computerChoice} beats ${humanChoice}! You lose!`);
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log(`${humanChoice} beats ${computerChoice}! You win!`);
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log(`${computerChoice} beats ${humanChoice}! You lose!`);
+        computerScore++;
+    } else if ()
+}
 
 console.log(`You have selected: ${humanChoice}`);
 console.log(`The computer has selected: ${computerChoice}`);
