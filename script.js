58e14a459a6eca51b749a1455ed1da6c9a9fd2e0
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-18 13:56:03 -0800

    Multi Round Logic
    -Added playGame function and moved playRound inside of it.

--- script.js
+++ script.js
@@ -20,57 +20,35 @@ function getHumanChoice() { // Logic for the user's selection.
 
 const computerChoice = getComputerSelection();
 const humanChoice = getHumanChoice();
-let humanScore = 0;
-let computerScore = 0;
-    
-function playRound(humanChoice, computerChoice) { // Logic for the response comparisons.
-   if ( // Conditions for the computer to win.
-        humanChoice === "rock" && computerChoice === "paper" || 
-        humanChoice === "paper" && computerChoice === "scissors" ||
-        humanChoice === "scissors" && computerChoice === "rock") {
-            ++humanScore;
 
-        console.log(`You win! ${humanChoice} beats ${computerChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
-    } else if ( // Conditions for the user to win.
-        humanChoice === "rock" && computerChoice === "scissors" ||
-        humanChoice === "paper" && computerChoice === "rock" ||
-        humanChoice === "scissors" && computerChoice === "paper") {
-            ++computerScore;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+        
+    function playRound(humanChoice, computerChoice) { // Logic for the response comparisons.
+    if ( // Conditions for the computer to win.
+            humanChoice === "rock" && computerChoice === "paper" || 
+            humanChoice === "paper" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "rock") {
+                ++humanScore;
 
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
-    } else if (humanChoice === computerChoice) { // Condition for a tie.
-        console.log(`It's a tie! ${computerChoice} is equal to ${humanChoice}! The score remains Player: ${humanScore} vs Computer: ${computerScore}`);
+            console.log(`You win! ${humanChoice} beats ${computerChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
+        } else if ( // Conditions for the user to win.
+            humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "paper" && computerChoice === "rock" ||
+            humanChoice === "scissors" && computerChoice === "paper") {
+                ++computerScore;
+
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
+        } else if (humanChoice === computerChoice) { // Condition for a tie.
+            console.log(`It's a tie! ${computerChoice} is equal to ${humanChoice}! The score remains Player: ${humanScore} vs Computer: ${computerScore}`);
+        };
+
+        return humanScore;
+        return computerScore;
     };
 
-    return humanScore;
-    return computerScore;
+    return playRound(humanChoice, computerChoice);
 };
 
-console.log(playRound(humanChoice, computerChoice));
-
-
-
-
-
-
-
-
-// TEST CODE
-
-// let num = 0;
-
-// let addNum = function(num) {
-//     num = num+1;
-//     return num;
-// }
-
-// function totalNum() {
-//     num = addNum(num);
-//     num = addNum(num);
-//     num = addNum(num);
-//     num = addNum(num);
-//     num = addNum(num);
-//     return num;
-// }
-
-// console.log(totalNum());
+console.log(playGame());
