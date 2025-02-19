4a5b9614e710d97aa0ffa585ee635be4ca87ee56
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-19 11:33:39 -0800

    Multi Round Logic
    - Moved the choice functions from global into the playGame function and change the variable type to let, rather than const
    - Nested the playRound function in the playGame function and made it a variable
    - Cleaned up returns with the help of the debugger. I didn't understand returns so I was putting them everywhere. EVERYWHERE. Learned a lot by using breakpoints and stepping through the code.
    - Starting from the 2nd time the choice functions are called, I changed the code so that the functions are called directly, rather than using the variable names. This fixed one of the issues preventing the code from running more than 1 round.

--- script.js
+++ script.js
@@ -1,86 +1,58 @@
-// function getComputerChoice() { // Logic for the computer's selection.
-//     let computerChoice = Math.floor(Math.random() * 3) + 1; // A random number is chosen between 1 and 3.
+function getComputerChoice() { // Logic for the computer's selection.
+    let computerChoice = Math.floor(Math.random() * 3) + 1; // A random number is chosen between 1 and 3.
     
-//     if (computerChoice === 1) { // Converts the numerical selection to a string.
-//         computerChoice = "rock";
-//     } else if (computerChoice === 2) {
-//         computerChoice = "paper";
-//     } else if (computerChoice === 3) {
-//         computerChoice = "scissors";
-//     };
+    if (computerChoice === 1) { // Converts the numerical selection to a string.
+        computerChoice = "rock";
+    } else if (computerChoice === 2) {
+        computerChoice = "paper";
+    } else if (computerChoice === 3) {
+        computerChoice = "scissors";
+    };
 
-//     return computerChoice;
-// }
-
-// function getHumanChoice() { // Logic for the user's selection.
-//     let humanChoice = prompt("Please choose 'Rock', 'Paper', or 'Scissors': ", " ").toLowerCase(); // Prompts the user to make a selection and converts the response to lowercase.
-
-//     return humanChoice;
-// }
-
-// const computerChoice = getComputerChoice();
-// const humanChoice = getHumanChoice();
-
-// function playGame() {
-//     let humanScore = 0;
-//     let computerScore = 0;
-        
-//     function playRound(humanChoice, computerChoice) { // Logic for the response comparisons.
-//     if ( // Conditions for the computer to win.
-//             humanChoice === "rock" && computerChoice === "paper" || 
-//             humanChoice === "paper" && computerChoice === "scissors" ||
-//             humanChoice === "scissors" && computerChoice === "rock") {
-//                 humanScore = ++humanScore;
-
-//             console.log(`You win! ${humanChoice} beats ${computerChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
-//         } else if ( // Conditions for the user to win.
-//             humanChoice === "rock" && computerChoice === "scissors" ||
-//             humanChoice === "paper" && computerChoice === "rock" ||
-//             humanChoice === "scissors" && computerChoice === "paper") {
-//                 computerScore = ++computerScore;
-
-//             console.log(`You lose! ${computerChoice} beats ${humanChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
-//         } else if (humanChoice === computerChoice) { // Condition for a tie.
-//             console.log(`It's a tie! ${computerChoice} is equal to ${humanChoice}! The score remains Player: ${humanScore} vs Computer: ${computerScore}`);
-//         };
-
-//         return humanScore;
-//         return computerScore;
-//     };
-
-//     return playRound(humanChoice, computerChoice);
-// };
-
-// console.log(playGame());
-
-
-function getComputerChoice() {
-    let computerChoice = "rock";
     return computerChoice;
 }
 
-function getHumanChoice() {
-    let humanChoice = "paper";
+function getHumanChoice() { // Logic for the user's selection.
+    let humanChoice = prompt("Please choose 'Rock', 'Paper', or 'Scissors': ", " ").toLowerCase(); // Prompts the user to make a selection and converts the response to lowercase.
+
     return humanChoice;
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+
 
 function playGame() {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
     let humanScore = 0;
     let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore = ++humanScore;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
-
-            return humanScore;
-        };
-
-        return playRound(humanChoice, computerChoice);
+        
+    let playRound = function(humanChoice, computerChoice) { // Logic for the response comparisons.
+        if ( // Conditions for the computer to win.
+                humanChoice === "rock" && computerChoice === "paper" || 
+                humanChoice === "paper" && computerChoice === "scissors" ||
+                humanChoice === "scissors" && computerChoice === "rock") {
+                computerScore = computerScore + 1;
+                return (`You lose! ${computerChoice} beats ${humanChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
+            } else if ( // Conditions for the user to win.
+                humanChoice === "rock" && computerChoice === "scissors" ||
+                humanChoice === "paper" && computerChoice === "rock" ||
+                humanChoice === "scissors" && computerChoice === "paper") {
+                humanScore = humanScore + 1;
+                return (`You win! ${humanChoice} beats ${computerChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
+            } else if (humanChoice === computerChoice) { // Condition for a tie.
+                return (`It's a tie! ${computerChoice} is equal to ${humanChoice}! The score remains Player: ${humanScore} vs Computer: ${computerScore}`);
+            } else {
+                return ("Invalid selection. Please try again.");
+            };
     };
-};
+        
+        console.log(playRound(humanChoice, computerChoice));
+        console.log(playRound(getHumanChoice(), getComputerChoice()));
+        console.log(playRound(getHumanChoice(), getComputerChoice()));
+        console.log(playRound(getHumanChoice(), getComputerChoice()));
+        console.log(playRound(getHumanChoice(), getComputerChoice()));
+
+
+    };
 
 playGame();
