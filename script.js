c0103ac208fa33c2184ba3422ab96458a3de4e21
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-03-11 10:52:18 -0700

    Player Selection Update
    - Player selection now works and successfully calls playRound. The game will get the player and computer selections, then store the round score.
    - Issue: When the player makes the same selection (e.g. rock, then rock again), the game does not make a new selection for the computer.

--- script.js
+++ script.js
@@ -1,57 +1,52 @@
-let computerChoice;
+let computerChoice = getComputerChoice();
 let humanChoice;
-// let humanScore = 0;
-// let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() { // Logic for the computer's selection.
+    let computerChoice = Math.floor(Math.random() * 3) + 1; // A random number is chosen between 1 and 3.
+    
+    if (computerChoice === 1) { // Converts the numerical selection to a string.
+        computerChoice = "rock";
+    } else if (computerChoice === 2) {
+        computerChoice = "paper";
+    } else if (computerChoice === 3) {
+        computerChoice = "scissors";
+    };
+
+    return computerChoice;
+};
 
 function getHumanChoice() {
-    const button = document.querySelectorAll("button").forEach(button => {
-            button.addEventListener("click", function() {
-                storeButtonId();
-            });
+    const buttons = document.querySelectorAll("button").forEach((button) => {
+        button.addEventListener("click", () => {
+            humanChoice = button.id;
+            playRound(humanChoice, computerChoice);
         });
+    });
+};
 
-            function storeButtonId() {
-                console.log(humanChoice = button.id);
-            };
+function playRound(humanChoice, computerChoice) { // Logic for the response comparisons.
+    if ( // Conditions for the computer to win.
+            humanChoice === "rock" && computerChoice === "paper" || 
+            humanChoice === "paper" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "rock") {
+            computerScore = computerScore + 1;
+            return console.log(`You lose! ${computerChoice} beats ${humanChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
+        } else if ( // Conditions for the user to win.
+            humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "paper" && computerChoice === "rock" ||
+            humanChoice === "scissors" && computerChoice === "paper") {
+            humanScore = humanScore + 1;
+            return console.log(`You win! ${humanChoice} beats ${computerChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
+        } else if (humanChoice === computerChoice) { // Condition for a tie.
+            return console.log(`It's a tie! ${computerChoice} is equal to ${humanChoice}! The score remains Player: ${humanScore} vs Computer: ${computerScore}`);
+        } else { // Invalid Selection
+            return console.log("Invalid selection. Please try again.");
+        };
+};
 
-            // function getComputerChoice() { // Logic for the computer's selection.
-            //     let computerChoice = Math.floor(Math.random() * 3) + 1; // A random number is chosen between 1 and 3.
-                
-            //     if (computerChoice === 1) { // Converts the numerical selection to a string.
-            //         computerChoice = "rock";
-            //     } else if (computerChoice === 2) {
-            //         computerChoice = "paper";
-            //     } else if (computerChoice === 3) {
-            //         computerChoice = "scissors";
-            //     };
-            
-            //     return computerChoice;
-            // };
-};    
-
-
-
-// function playRound(humanChoice, computerChoice) { // Logic for the response comparisons.
-//     if ( // Conditions for the computer to win.
-//             humanChoice === "rock" && computerChoice === "paper" || 
-//             humanChoice === "paper" && computerChoice === "scissors" ||
-//             humanChoice === "scissors" && computerChoice === "rock") {
-//             computerScore = computerScore + 1;
-//             return (`You lose! ${computerChoice} beats ${humanChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
-//         } else if ( // Conditions for the user to win.
-//             humanChoice === "rock" && computerChoice === "scissors" ||
-//             humanChoice === "paper" && computerChoice === "rock" ||
-//             humanChoice === "scissors" && computerChoice === "paper") {
-//             humanScore = humanScore + 1;
-//             return (`You win! ${humanChoice} beats ${computerChoice}! The score is now Player: ${humanScore} vs Computer: ${computerScore}`);
-//         } else if (humanChoice === computerChoice) { // Condition for a tie.
-//             return (`It's a tie! ${computerChoice} is equal to ${humanChoice}! The score remains Player: ${humanScore} vs Computer: ${computerScore}`);
-//         } else { // Invalid Selection
-//             return ("Invalid selection. Please try again.");
-//         };
-// };
-
-
+getHumanChoice();
 
         // // Gameplay Rounds
         // console.log('Round 1! FIGHT!!!');
@@ -69,13 +64,13 @@ function getHumanChoice() {
         // console.log('Round 5! FIGHT!!!');
         // console.log(playRound(getHumanChoice(), getComputerChoice()));
 
-        // function winner (humanScore, computerScore) { // Announce Winner Logic
-        //     if (humanScore > computerScore) {
-        //         return (`The game is over! The player has ${humanScore} points and the computer has ${computerScore} points. The player wins the game!`);
-        //     } else if (humanScore < computerScore) {
-        //         return (`The game is over! The player has ${humanScore} points and the computer has ${computerScore} points. The computer wins the game!`);
-        //     } else {
-        //         return (`The game is over! The player has ${humanScore} points and the computer has ${computerScore} points. The game ends in a tie!`);
-        //     };
-        // };
-        // console.log(winner(humanScore, computerScore));
+// function winner (humanScore, computerScore) { // Announce Winner Logic
+//     if (humanScore > computerScore) {
+//         return (`The game is over! The player has ${humanScore} points and the computer has ${computerScore} points. The player wins the game!`);
+//     } else if (humanScore < computerScore) {
+//         return (`The game is over! The player has ${humanScore} points and the computer has ${computerScore} points. The computer wins the game!`);
+//     } else {
+//         return (`The game is over! The player has ${humanScore} points and the computer has ${computerScore} points. The game ends in a tie!`);
+//     };
+// };
+// console.log(winner(humanScore, computerScore));
